0fb4e637563943d04d56240537a84bfc6b0458c1
Arnoldcsik <arnoldcsik32@gmail.com>
2026-03-17 20:21:21 +0100

    Add files via upload

---
+++ Teszteles/Program1/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Program1
+{
+
+    using System;
+
+    public class SimpleProgram
+    {
+        public int Method1(string text)
+        {
+            return text.Length;
+        }
+
+        public string Method2(string text)
+        {
+            return text.ToUpper();
+        }
+
+        public string Method3(string text)
+        {
+            return text.ToLower();
+        }
+
+        public bool Method4(string text)
+        {
+            return text.Contains("a");
+        }
+
+        public int Method5(string text)
+        {
+            int count = 0;
+
+            foreach (char c in text)
+            {
+                if (c == 'a')
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public string Method6(string firstName, string lastName)
+        {
+            return firstName + " " + lastName;
+        }
+
+        public bool Method7(int number)
+        {
+            return number > 0;
+        }
+
+        public bool Method8(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public string Method9(string text)
+        {
+            char[] letters = text.ToCharArray();
+            Array.Reverse(letters);
+            return new string(letters);
+        }
+
+        public int Method10(int[] numbers)
+        {
+            int count = 0;
+
+            foreach (int n in numbers)
+            {
+                if (n > 10)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Teszteles/Program1Teszteles/UnitTest1.cs
@@ -0,0 +1,81 @@
+﻿namespace Program1Teszteles;
+using Program1;
+
+
+    public class UnitTest1
+{
+    [Fact]
+    public void Method1()
+    {
+        var program = new SimpleProgram();
+        int result = program.Method1("Hello");
+        Assert.Equal(5, result);
+    }
+    
+    [Fact]
+    public void Method2()
+    {
+        var program = new SimpleProgram();
+        string result = program.Method2("Hello");
+        Assert.Equal("HELLO", result);
+    }
+
+    [Fact]
+    public void Method3() {
+        var program = new SimpleProgram();
+        string result = program.Method3("Hello");
+        Assert.Equal("hello", result);
+    }
+    [Fact]
+    public void Method4()
+    {
+        var program = new SimpleProgram();
+        bool result = program.Method4("alma");
+        Assert.True(true);
+    }
+    [Fact]
+    public void Method5()
+    {
+        var program = new SimpleProgram();
+        int result = program.Method5("banana");
+        Assert.Equal(3, result);
+    }
+    [Fact]
+    public void Method6()
+    {
+        var program = new SimpleProgram();
+        string result = program.Method6("Banana", "Apple");
+        Assert.Equal("Banana Apple", result);
+    }
+    [Fact]
+    public void Method7()
+    {
+        var program = new SimpleProgram();
+        bool result = program.Method7(7);
+        Assert.True(true);
+    }
+    [Fact]
+    public void Method8()
+    {
+        var program = new SimpleProgram();
+        bool result = program.Method8(11);
+        Assert.False(false);
+    }
+    [Fact]
+    public void Method9()
+    {
+        var program = new SimpleProgram();
+        string result = program.Method9("valami");
+        Assert.Equal("imalav", result);
+    }
+    [Fact]
+    public void Method10()
+    {
+        var program = new SimpleProgram();
+        int result = program.Method10(new int[] { 5, 11, 12, 13, 0 });
+        Assert.Equal(3, result);
+
+    }
+}
+
+
